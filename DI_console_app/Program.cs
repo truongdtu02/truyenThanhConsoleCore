10d2684cf9df291ff5629f49d2622c2681e20e2e
truonghd1 <truongdtu02@gmail.com>
2022-04-08 10:48:00 +0700

    use DI_console_app to test repeate task scheme

--- DI_console_app/Program.cs
+++ DI_console_app/Program.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 //DI, Serilog and settings
 
@@ -11,31 +13,63 @@ namespace DI_console_app
 {
     class Program
     {
+        static void repeatTaskUseStopWatch()
+        {
+            //launch timer
+            Stopwatch stopWatchSend = new Stopwatch();
+            stopWatchSend.Start();
+
+            long count = 1;
+            long interval = 600, remainTime = 0;
+
+            while(true)
+            {
+                double i = 0;
+                for(double j = 0; j < 1000; j++)
+                {
+                    i += (j + 2 * j + j * j);
+                    Random random = new Random();
+                    i += (double)random.Next();
+                }
+                remainTime = count * interval - (long)stopWatchSend.Elapsed.TotalMilliseconds;
+                count++;
+                Console.WriteLine($"Remain time {remainTime}. Res {i}. Count {count}");
+                if(remainTime > 0)
+                {
+                    Thread.Sleep((int)remainTime);
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            repeatTaskUseStopWatch();
+
             //Console.WriteLine("Hello World!");
-            var builder = new ConfigurationBuilder();
-            BuildConfig(builder);
+            //var builder = new ConfigurationBuilder();
+            //BuildConfig(builder);
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Build())
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateLogger();
+            //Log.Logger = new LoggerConfiguration()
+            //    .ReadFrom.Configuration(builder.Build())
+            //    .Enrich.FromLogContext()
+            //    .WriteTo.Console()
+            //    .CreateLogger();
 
-            Log.Logger.Information("Application Starting");
+            //Log.Logger.Information("Application Starting");
 
-            var host = Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddTransient<IGreetingService ,GreetingService>(); //give me an instance every time I ask for
-                })
-                .UseSerilog()
-                .Build();
+            //var host = Host.CreateDefaultBuilder()
+            //    .ConfigureServices((context, services) =>
+            //    {
+            //        services.AddTransient<IGreetingService ,GreetingService>(); //give me an instance every time I ask for
+            //    })
+            //    .UseSerilog()
+            //    .Build();
 
-            //var svc = ActivatorUtilities.CreateInstance<IGreetingService>(host.Services);
-            var svc = host.Services.GetRequiredService<IGreetingService>();
-            svc.Run();
+            ////var svc = ActivatorUtilities.CreateInstance<IGreetingService>(host.Services);
+            //var svc = host.Services.GetRequiredService<IGreetingService>();
+            //svc.Run();
+
+
+
 
         }
 
